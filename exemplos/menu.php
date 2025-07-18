e170f779323343a61487146e08b951e7d52d0e45
Micael Junco <micaeljuncoplay4@gmail.com>
2025-07-18 15:49:11 -0300

    Exemplo do uso do include com PHP

---
+++ exemplos/menu.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Menu PHP</title>
+</head>
+<body>
+    <div class="menu">
+        <h1>Bem-vindo à Página</h1>
+        <p>Um menu aqui.</p>
+        <?php include("links.html"); echo "<br>Micael Jeferson Junco<br>";?>
+    </div>
+</body>
+</html>
