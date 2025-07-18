8732e2c2c21f8ce2056c5c7ae91278979b03ce9a
Micael Junco <micaeljuncoplay4@gmail.com>
2025-07-18 15:48:46 -0300

    Demonstração de atribuição de array e lista

---
+++ exemplos/lista.php
@@ -0,0 +1,9 @@
+<?php
+$valores = array("Micael","16","M","9999") ;
+list($usuario, $idade, $genero, $qi) = $valores;
+echo"Usuário: ".$usuario;
+echo"Idade: ".$idade;
+echo "Gênero: ".$genero;
+echo "QI: ".$qi;
+
+echo "<br> Micael Jeferson Junco <br>";
