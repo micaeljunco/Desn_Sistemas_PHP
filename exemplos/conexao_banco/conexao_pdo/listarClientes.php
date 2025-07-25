59dad0a1f94d2365e41dad0fb18448c98eead27f
Micael Junco <micael.j.junco@gmail.com>
2025-07-25 17:14:47 -0300

    Create listarClientes.php
    frontend da listagem de clientes em php

---
+++ exemplos/conexao_banco/conexao_pdo/listarClientes.php
@@ -0,0 +1,38 @@
+<?php
+require 'conexao.php';
+
+$conexao = conectarBanco();
+$stmt = $conexao->prepare('SELECT * FROM cliente');
+$stmt->execute();
+$clientes = $stmt->fetchAll();
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lista de Clientes</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h2>Lista Clientes</h2>
+    <table>
+        <tr>
+            <th>ID</th>
+            <th>Nome</th>
+            <th>Endereço</th>
+            <th>Telefone</th>
+            <th>Email</th>            
+        </tr>
+        <?php foreach ($clientes as $cliente): ?>
+        <tr>
+            <td> <?= htmlspecialchars($cliente["id_cliente"]) ?></td>
+            <td> <?= htmlspecialchars($cliente["nome"]) ?></td>
+            <td> <?= htmlspecialchars($cliente["endereco"]) ?></td>
+            <td> <?= htmlspecialchars($cliente["telefone"]) ?></td>
+            <td> <?= htmlspecialchars($cliente["email"]) ?></td>
+        </tr>
+        <?php endforeach; ?>
+    </table>
+</body>
+</html>
