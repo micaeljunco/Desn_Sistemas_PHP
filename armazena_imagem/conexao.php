9f38531bf8d2aa618c3786cadf9587e546fa45a9
Micael Junco <micael.j.junco@gmail.com>
2025-08-01 17:02:45 -0300

    arquivo de conexao mysqli
    Arquivo de conexao mysqli para o projeto de banco de dados com imagens

---
+++ armazena_imagem/conexao.php
@@ -0,0 +1,14 @@
+<?php
+// Habilita relatório detalhado dos erros no MySQL
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+
+// Configuração do banco de dado:
+$endereco = "localhost";    // Endereço do servidor MySQL
+$usuario = "root";          // Nome do usuário do banco de dados
+$senha = "";                // Senha do usuário
+$banco = "armazena_imagem";         // Nome do banco de dados
+
+$conexao = mysqli_connect($endereco, $usuario, $senha, $banco);
+if ($conexao -> connect_error) {
+    die("[ERRO]: Impossível conectar-se ao banco de dados". $conexao -> connect_error);
+}
