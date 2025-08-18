b22986df986e975dbe3c97ebc8853c9db8986aee
Micael Junco <micael.j.junco@gmail.com>
2025-08-18 16:57:55 -0300

    Validação básica para campos do cad de usuário
    Fiz uma função para validar os campos dos formularios de cadastro e alteração de usuários

--- senai_login/validacoes.js
+++ senai_login/validacoes.js
@@ -22,3 +22,38 @@ function validarFuncionario() {
 
     return true;
 }
+
+function validarCamposUsuario() {
+    let camposInvalidos = 0;
+    let campoNome = document.getElementById('nome');
+    let campoEmail = document.getElementById('email');
+    let campoSenha = document.getElementById('senha');
+    let campoNovaSenha = document.getElementById('nova_senha');
+
+    if (campoNome.value.length <= 0 || campoNome.value.length > 100) {
+        camposInvalidos += 1;
+        alert ("Campo \"Nome\" inválido.")
+    }
+
+    if (campoEmail.value.length <= 5 || campoEmail.value.length > 100) {
+        camposInvalidos += 1;
+        alert ("Campo \"Email\" inválido.")
+    }
+
+    if (campoSenha) {
+        if (campoSenha.value.length < 8 || campoSenha.value.length > 255) {
+            camposInvalidos += 1;
+            alert ("Campo \"Senha\" inválido.")
+        }        
+    }
+
+    // Para funcionar para o arquivo de alteração também
+    if (campoNovaSenha && campoNovaSenha.value.length > 0) {
+        if (campoNovaSenha.value.length < 8 || campoNovaSenha.value.length > 255) {
+            camposInvalidos += 1;
+            alert ("Campo \"Nova Senha\" inválido.")
+        }
+    }
+
+    return camposInvalidos === 0;
+}
